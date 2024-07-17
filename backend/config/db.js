b81783e0855a64c84ddb4f5149f2d2b871e35727
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-07-17 08:10:34 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://sbhunia2903:sayak2903@cluster0.zdfde7x.mongodb.net/foodo').then(()=>console.log("DB Connected"));
    
 }
 
